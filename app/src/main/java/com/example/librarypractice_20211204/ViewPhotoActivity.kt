1b448d1f44d9c37f19f10d7f82560ef23263d308
김창배 <cbkim93@naver.com>
2021-12-04 15:52:26 +0900

    PhotoView 라이브러리 체험  - 설치법 복잡한 편임.

--- app/src/main/java/com/example/librarypractice_20211204/ViewPhotoActivity.kt
+++ app/src/main/java/com/example/librarypractice_20211204/ViewPhotoActivity.kt
@@ -2,10 +2,13 @@ package com.example.librarypractice_20211204
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_view_photo.*
 
 class ViewPhotoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_photo)
+
+
     }
 }
