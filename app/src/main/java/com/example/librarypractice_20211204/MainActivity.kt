bf7fe46306d93e1d5a6b5666929d026a7644bb5c
김창배 <cbkim93@naver.com>
2021-12-04 16:21:50 +0900

    TedPermission 라이브러리 활용

--- app/src/main/java/com/example/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_20211204/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.librarypractice_20211204
 
+import android.Manifest
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +24,21 @@ class MainActivity : AppCompatActivity() {
         circleImageView.setOnClickListener {
             val myIntent = Intent(this, ViewPhotoActivity::class.java)
             startActivity(myIntent)
+        }
+
+        btnCall.setOnClickListener {
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+                    val myUri = Uri.parse("tel:010-5555-9999")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(this@MainActivity, "권한이 없어 전화 연결 불가", Toast.LENGTH_SHORT).show()
+                }
+            }
+            TedPermission.create().setPermissionListener(pl).setPermissions( Manifest.permission.CALL_PHONE ).check()
         }
     }
 
