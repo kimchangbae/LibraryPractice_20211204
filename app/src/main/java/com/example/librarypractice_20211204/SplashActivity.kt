fd2396ac6614905cffa1941b81af4b6d45a75268
김창배 <cbkim93@naver.com>
2021-12-11 11:44:31 +0900

    메인 화면으로 2초 지나면 이동 (복습)

--- app/src/main/java/com/example/librarypractice_20211204/SplashActivity.kt
+++ app/src/main/java/com/example/librarypractice_20211204/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.example.librarypractice_20211204
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +15,13 @@ class SplashActivity : AppCompatActivity() {
     }
 
     fun setupValues() {
+        // 2초가 지나면 메인화면으로 이동
 
+        val handler = Handler( Looper.getMainLooper() )
+        handler.postDelayed({
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            finish()
+        },2000)
     }
 }
