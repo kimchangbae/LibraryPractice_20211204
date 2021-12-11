a7eeb7c7c211d47d86f366a660fa73aeb366ff4f
김창배 <cbkim93@naver.com>
2021-12-11 11:22:45 +0900

    앱 시작화면 -> SplachActivity로 변경

---
+++ app/src/main/java/com/example/librarypractice_20211204/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.librarypractice_20211204
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        setupValues()
+    }
+
+    fun setupValues() {
+
+    }
+}
