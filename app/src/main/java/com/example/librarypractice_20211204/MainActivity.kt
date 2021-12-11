d19b8e6823004a41fc11ecfcb56ea88c6b4e2c5b
김창배 <cbkim93@naver.com>
2021-12-11 10:45:15 +0900

    Glide 라이브러리 체험 - 인터넷 권한 열어줘야 가능함.

--- app/src/main/java/com/example/librarypractice_20211204/MainActivity.kt
+++ app/src/main/java/com/example/librarypractice_20211204/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.gun0912.tedpermission.PermissionListener
 import com.gun0912.tedpermission.normal.TedPermission
 import kotlinx.android.synthetic.main.activity_main.*
@@ -38,11 +39,24 @@ class MainActivity : AppCompatActivity() {
                     Toast.makeText(this@MainActivity, "권한이 없어 전화 연결 불가", Toast.LENGTH_SHORT).show()
                 }
             }
-            TedPermission.create().setPermissionListener(pl).setPermissions( Manifest.permission.CALL_PHONE ).check()
+            TedPermission.create()
+                .setPermissionListener(pl)
+                .setPermissions(
+                    Manifest.permission.CALL_PHONE
+                    , Manifest.permission.ACCESS_MEDIA_LOCATION)
+                .check()
         }
     }
 
     private fun setValues() {
+        // 인터넷 상에 있는 이미지를 -> 곧바로 이미지뷰에 적용.
+        // https://www.theguru.co.kr/data/photos/20210937/art_16316071303022_bf8378.jpg
 
+        // 이미지 주소를 변수 에 담아두자.
+        val imageURL = "https://www.theguru.co.kr/data/photos/20210937/art_16316071303022_bf8378.jpg"
+        Glide
+            .with( this )
+            .load( imageURL )
+            .into( imgInternet )
     }
 }
